242ff9db8202da84a349bbfabf458375db44a37c
JavaJabba <36043808+JavaJabba@users.noreply.github.com>
2022-11-09 16:19:00 +0000

    lecture

---
+++ priority_queue.py
@@ -0,0 +1,19 @@
+
+
+class Element:
+
+    def __init__(self, key) -> None:
+        pass
+        
+        
+    def add():
+        return
+    
+    def min():
+        return
+    
+    def remove_min():
+        return
+    
+    def length():
+        return
