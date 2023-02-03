4e7273a9b5afc806cb4becbf4ef6307d7515a208
JavaJabba <dylanmurray1997@gmail.com>
2023-02-03 16:23:53 +0000

    w

--- Semester_Two/lab_one.py
+++ Semester_Two/lab_one.py
@@ -33,7 +33,15 @@ class BinaryHeap:
 
     def add(self, key, value):
         ''' add (key,value) to the heap '''
-        pass
+        self.item = (key, value)
+        self._heap.append(self.item)
+        self._parentNode = self._heap[(self.item-1)/2]
+        self._rightChild = self._heap[(self.item*2)+2]
+        self._leftChild = self._heap[(self.item*2)+1]
+        
+        if self._heap[self.item] > self._heap[parentNode]:
+            tempVal =self._parentNode
+            self._parent
 
     def max(self):
         ''' return max value (top of heap) '''
@@ -62,3 +70,26 @@ class BinaryHeap:
 
     def _downHeap(self):
         pass
+
+
+    def _testadd():
+        # print('Testing that we can add items to an array-based binary heap PQ')
+        pq = BinaryHeap()
+        # print('pq has size:', pq.length(), '(should be 0)')
+        pq.add(25,'25')
+        pq.add(4, '4')
+        # print('pq has size:', pq.length(), '(should be 2)')
+        # print(pq, '(should be 4,25, could also show index and value)')
+        pq.add(19,'19')
+        # pq.add(12,'12')
+        # print(pq, '(should be 4,12,19,25)')
+        # pq.add(17,'17')
+        # pq.add(8,'8')
+        # print(pq, '(should be 4,12,8,25,17,19)')
+        # print('pq length:', pq.length(), '(should be 6)')
+        # print('pq max item:', pq.max(), '(should be 4)')
+        # print()
+        print(pq._heap)
+        return pq
+
+BinaryHeap._testadd()
